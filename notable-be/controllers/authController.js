3bbb4a7187d403213c5ffbe6588256073323acea
Barum Niazi <96373839+Barum-Niazi@users.noreply.github.com>
2024-08-23 05:32:02 +0500

    set up mongoose connection, created auth controller, set up routes

---
+++ notable-be/controllers/authController.js
@@ -0,0 +1,34 @@
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+module.exports.register = async (req, res) => {
+    const { username, password, email } = req.body;
+    const user = new User({
+        username,
+        password,
+        email,
+    });
+
+    let salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    try {
+        await user.save();
+        res.status(201).send("User created: " + user.username);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+};
+
+module.exports.login = async (req, res) => {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return res.status(400).send("User not found");
+
+    const matchedPassword = await bcrypt.compare(
+        req.body.password,
+        user.password
+    );
+    if (!matchedPassword) return res.status(400).send("Invalid password");
+
+    res.status(200).send("Logged in");
+};
--- notable-be/index.js
+++ notable-be/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const router = require("./routes/api/auth");
+app.use(bodyParser.json());
 
+mongoose
+    .connect("mongodb://localhost:27017/notable", {})
+    .then((result) => {
+        console.log("Connected to database");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.use("/api/auth", router);
 app.get("/", (req, res) => {
     res.send("Notable");
 });
